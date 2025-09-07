453164682ec7cee222f3f1da87684bc94c552fce
User <user@example.com>
2025-09-07 11:37:02 +0000

    Отключение ESLint правила no-explicit-any для PrismaticBurst
    - Добавлена директива eslint-disable для типа any
    - Необходимо для работы с WebGL и библиотекой OGL
    - Теперь ESLint сборка должна пройти успешно

--- src/components/PrismaticBurst.tsx
+++ src/components/PrismaticBurst.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useEffect, useRef } from 'react';
 import { Renderer, Program, Mesh, Triangle, Texture } from 'ogl';
 import './PrismaticBurst.css';
